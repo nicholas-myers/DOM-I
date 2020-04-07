0fc2299d6fe8e973088847d19f0d5cb79becac1a
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-06 21:12:06 -0700

    added buttons to change content

--- js/index.js
+++ js/index.js
@@ -49,6 +49,10 @@ logo.style.filter = "invert(100%)";
 
 //////////// GET LINKS /////////////////////
 const nav = document.querySelector("nav");
+nav.style.display = "flex";
+nav.style.justifyContent = "space-between";
+nav.style.alignItems = "center";
+
 // BLOG
 const blogLink = document.createElement("a");
 blogLink.href = "#Blog";
@@ -68,30 +72,29 @@ for (let i = 0; i < links.length; i++) {
   link.style.textDecoration = "none";
   link.style.borderRadius = "10px";
   link.style.padding = "5px";
-
+  link.style.fontWeight = "bold";
 
   link.addEventListener("mouseover", (event) => {
-  event.target.style.color = "white";
-  event.target.style.backgroundColor = "green";
-  
-});
+    event.target.style.color = "white";
+    event.target.style.backgroundColor = "green";
+  });
   link.addEventListener("mouseout", (event) => {
     event.target.style.color = "green";
     event.target.style.backgroundColor = "navy";
   });
 }
 
-const link1 = document.querySelector('nav a:nth-of-type(2)');
+const link1 = document.querySelector("nav a:nth-of-type(2)");
 link1.textContent = siteContent["nav"]["nav-item-1"];
-const link2 = document.querySelector('nav a:nth-of-type(3)');
+const link2 = document.querySelector("nav a:nth-of-type(3)");
 link2.textContent = siteContent["nav"]["nav-item-2"];
-const link3 = document.querySelector('nav a:nth-of-type(4)');
+const link3 = document.querySelector("nav a:nth-of-type(4)");
 link3.textContent = siteContent["nav"]["nav-item-3"];
-const link4 = document.querySelector('nav a:nth-of-type(5)');
+const link4 = document.querySelector("nav a:nth-of-type(5)");
 link4.textContent = siteContent["nav"]["nav-item-4"];
-const link5 = document.querySelector('nav a:nth-of-type(6)');
+const link5 = document.querySelector("nav a:nth-of-type(6)");
 link5.textContent = siteContent["nav"]["nav-item-5"];
-const link6 = document.querySelector('nav a:nth-of-type(7)');
+const link6 = document.querySelector("nav a:nth-of-type(7)");
 link6.textContent = siteContent["nav"]["nav-item-6"];
 // for (let i = 0; i < links.length; i++) {
 //   const linkContent = Object.values(siteContent["nav"]);
@@ -116,19 +119,13 @@ header.style.borderBottomRightRadius = "10px";
 // nav.addEventListener("mouseover", (event) => {
 //   event.target.style.color = "white";
 //   event.target.style.backgroundColor = "lightblue";
-  
+
 // });
 
 // nav.addEventListener("mouseout", (event) => {
 //   event.target.style.color = "green";
 //   event.target.style.backgroundColor = "navy";
 // });
-
-nav.style.display = "flex";
-nav.style.justifyContent = "space-around";
-nav.style.alignItems = "center";
-
-
 
 /////////////// GET CTA IMG ////////////////////
 const ctaImg = document.getElementById("cta-img");
@@ -147,6 +144,7 @@ ctaTitle.textContent = siteContent["cta"]["h1"];
 ctaButton.textContent = siteContent["cta"]["button"];
 
 ///////////////// GET MAIN CONTENT ////////////////
+
 
 // TITLES //
 const featuresTitle = document.querySelector(".top-content div:first-child h4");
@@ -183,6 +181,73 @@ aboutContent.textContent = siteContent["main-content"]["about-content"];
 servicesContent.textContent = siteContent["main-content"]["services-content"];
 productContent.textContent = siteContent["main-content"]["product-content"];
 visionContent.textContent = siteContent["main-content"]["vision-content"];
+
+///create new buttons
+const featuresButton = document.createElement("button");
+featuresButton.textContent = "Find Out More";
+featuresButton.style.padding = "5px";
+featuresButton.style.fontSize = "1rem";
+const featuresDiv = document.querySelector('.top-content div:first-child');
+featuresDiv.appendChild(featuresButton);
+
+featuresButton.addEventListener("click", () => {
+  featuresContent.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+});
+
+const aboutButton = document.createElement("button");
+aboutButton.textContent = "Find Out More";
+aboutButton.style.padding = "5px";
+aboutButton.style.fontSize = "1rem";
+const aboutDiv = document.querySelector('.top-content div:last-child');
+aboutDiv.appendChild(aboutButton);
+
+aboutButton.addEventListener("click", () => {
+  aboutContent.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+});
+
+const servicesButton = document.createElement("button");
+servicesButton.textContent = "Find Out More";
+servicesButton.style.padding = "5px";
+servicesButton.style.fontSize = "1rem";
+const servicesDiv = document.querySelector('.bottom-content div:first-child');
+servicesDiv.appendChild(servicesButton);
+
+servicesButton.addEventListener("click", () => {
+  servicesContent.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+});
+
+
+const productButton = document.createElement("button");
+productButton.textContent = "Find Out More";
+productButton.style.padding = "5px";
+productButton.style.fontSize = "1rem";
+const productDiv = document.querySelector('.bottom-content div:nth-of-type(2)');
+productDiv.appendChild(productButton);
+
+productButton.addEventListener("click", () => {
+  productContent.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+});
+
+
+const visionButton = document.createElement("button");
+visionButton.textContent = "Find Out More";
+visionButton.style.padding = "5px";
+visionButton.style.fontSize = "1rem";
+const visionDiv = document.querySelector('.bottom-content div:last-child');
+visionDiv.appendChild(visionButton);
+
+visionButton.addEventListener("click", () => {
+  visionContent.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+});
+
+
+
+
+
+
+
+
+
 
 //////////////////////// GET CONTACT ///////////////////////////
 
